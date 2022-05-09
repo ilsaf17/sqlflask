eefc5755db51f1b1889e53a420af6384e3bdabd7
ilsaf17 <timeforacupoftea@outlook.com>
2022-05-09 21:06:16 +0300

    9

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 from requests import get, post, delete
 
-print(get('http://localhost:5000/api/jobs/1').json())
-print(get('http://localhost:5000/api/jobs').json())
 
+print(get('http://localhost:5000/api/jobs').json())
+print(get('http://localhost:5000/api/jobs/1').json())
+# неверно:
+print(get('http://localhost:5000/api/jobs/999').json())
+print(get('http://localhost:5000/api/jobs/dfds').json())
 # print(get('http://localhost:5000/api/users/show/1').json())
 # print(get('http://localhost:5000/api/jobs').json())
 # print(get('http://localhost:5000/api/jobs/1').json())
