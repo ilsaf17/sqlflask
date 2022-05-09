4f910c2252ddaedcbb714f37a22108306afa67be
ilsaf17 <timeforacupoftea@outlook.com>
2022-05-09 21:23:14 +0300

    11

--- test.py
+++ test.py
@@ -1,11 +1,19 @@
 from requests import get, post, delete
 
-print(post('http://localhost:5000/api/jobs',
-           json={'team_leader': 2,
-                 'job': 'Текст новости1112',
-                 'work_size': 12
-                 }).json())
+print(delete('http://localhost:5000/api/jobs/1').json())
 
+# print(post('http://localhost:5000/api/jobs',
+#            json={'team_leader': 2,
+#                  'job': 'Текст новости1112',
+#                  'work_size': 12
+#                  }).json())
+# не ввели все необходимые параметры
+# print(post('http://localhost:5000/api/jobs',
+#            json={'team_leader': 2,
+#                  'job': 'Текст новости1112'
+#                  }).json())
+# убедимся что работа добавлена
+# print(get('http://localhost:5000/api/jobs').json())
 # print(get('http://localhost:5000/api/jobs').json())
 # print(get('http://localhost:5000/api/jobs/1').json())
 # # неверно:
@@ -17,10 +25,6 @@ print(post('http://localhost:5000/api/jobs',
 # print(get('http://localhost:5000/api/jobs/10').json())
 # print(get('http://localhost:5000/api/jobs/sdsdwsd').json())
 # print(get('http://localhost:5000/api/users').json())
-#
-
-# print(delete('http://localhost:5000/api/users/3').json())
-#
 # print(post('http://localhost:5000/api/users',
 #            json={'id': 4,
 #                'name': 'Заголовок112',
