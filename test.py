7827c75c16b9a18dba547228c111b9118103a5d9
ilsaf17 <timeforacupoftea@outlook.com>
2022-05-10 18:08:15 +0300

    17

--- test.py
+++ test.py
@@ -1,5 +1,29 @@
 from requests import get, post, delete
 
+# TEST jobs_resource
+# correct:
+print(get('http://localhost:5000/api/jobs').json())
+print(get('http://localhost:5000/api/jobs/1').json())
+print(post('http://localhost:5000/api/jobs',
+           json={'team_leader': 2,
+                 'job': 'Текст новости1112',
+                 'work_size': 1, }).json())
+
+print(delete('http://localhost:5000/api/jobs/1').json())
+# incorect:
+# undefined id:
+print(get('http://localhost:5000/api/jobs/999').json())
+# hasnt defined all arguments in post
+print(post('http://localhost:5000/api/jobs',
+           json={'team_leader': 2,
+                 'job': 'Текст новости1112'
+                 }).json())
+# empty json
+print(post('http://localhost:5000/api/jobs',
+           json={}).json())
+# undefined id deleting
+print(delete('http://localhost:5000/api/jobs/999').json())
+
 # TEST users_resource
 # correct:
 print(get('http://localhost:5000/api/users').json())
@@ -23,14 +47,3 @@ print(post('http://localhost:5000/api/users',
            json={}).json())
 # undefined id deleting
 print(delete('http://localhost:5000/api/users/999').json())
-
-
-
-
-
-# не ввели все необходимые параметры
-# print(post('http://localhost:5000/api/jobs',
-#            json={'team_leader': 2,
-#                  'job': 'Текст новости1112'
-#                  }).json())
-
